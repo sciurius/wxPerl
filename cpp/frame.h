93d4dad6baf0e8a5a1f40300fbb7781654f76db5
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2001-01-01 22:34:40 +0000

    fixed OnCreateToolBar
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@55 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- cpp/frame.h
+++ cpp/frame.h
@@ -50,7 +50,7 @@ wxToolBar* _wxFrame::OnCreateToolBar( long style, wxWindowID id, const wxString&
     if( m_callback.FindCallback( "OnCreateToolBar" ) ) 
     {
         SV* ret = m_callback.CallCallback( G_SCALAR, "llp", style, id, name.c_str() );
-        wxToolBar* retval = (wxToolBar*)_sv_2_object( ret, "Wx::ToolBar" );
+        wxToolBar* retval = (wxToolBar*)_sv_2_object( ret, "Wx::ToolBarSimple" );
         SvREFCNT_dec( ret );
 
         return retval;
