e10ec7fd6f1a1d155ee092d65cfecdb39cf78474
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2001-02-03 20:45:59 +0000

    2.3 API additions
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@88 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

---
+++ lib/Wx/Locale.pm
@@ -0,0 +1,29 @@
+#############################################################################
+## Name:        Locale.pm
+## Purpose:     Wx::Locale
+## Author:      Mattia Barbon
+## Modified by:
+## Created:      2/ 2/2001
+## RCS-ID:      
+## Copyright:   (c) 2001 Mattia Barbon
+## Licence:     This program is free software; you can redistribute it and/or
+##              modify it under the same terms as Perl itself
+#############################################################################
+
+package Wx::Locale;
+
+push @Wx::EXPORT_OK, qw(_);
+
+*Wx::_ = \&Wx::GetTranslation;
+
+sub new {
+  shift;
+
+  #FIXME// this should be conditionally defined
+  Wx::_match( @_, $Wx::_n_n, 2 )          && return Wx::Locale::newShort( @_ );
+  Wx::_match( @_, $Wx::_s_s_s_b_b, 1, 1 ) && return Wx::Locale::newLong( @_ );
+}
+
+# Local variables: #
+# mode: cperl #
+# End: #
