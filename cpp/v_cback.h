ce2f27974b7424af2dd3266f7b6c21e8dd4fa86f
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2001-06-02 16:47:32 +0000

    added more helper macros
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@262 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- cpp/v_cback.h
+++ cpp/v_cback.h
@@ -21,6 +21,7 @@ public:
     bool FindCallback( const char* name );
     SV* CallCallback( I32 flags, const char* argtypes,
                       va_list& arglist );
+    SV* GetMethod() { return m_method; }
 public:
     const char* m_package;
     HV* m_stash;
@@ -38,6 +39,15 @@ inline wxPliVirtualCallback::wxPliVirtualCallback( const char* package ) {
 #define DEC_V_CBACK_BOOL__VOID( METHOD ) \
   bool METHOD();
 
+#define DEC_V_CBACK_VOID__VOID( METHOD ) \
+  void METHOD();
+
+#define DEC_V_CBACK_BOOL__INT( METHOD ) \
+  bool METHOD( int );
+
+#define DEC_V_CBACK_BOOL__INT_INT( METHOD ) \
+  bool METHOD( int, int );
+
 #define DEF_V_CBACK_BOOL__VOID( CLASS, BASE, METHOD ) \
   bool CLASS::METHOD()                                                        \
   {                                                                           \
@@ -49,6 +59,59 @@ inline wxPliVirtualCallback::wxPliVirtualCallback( const char* package ) {
         return val;                                                           \
     } else                                                                    \
         return BASE::METHOD();                                                \
+  }
+
+#define DEF_V_CBACK_VOID__VOID( CLASS, BASE, METHOD ) \
+  void CLASS::METHOD()                                                        \
+  {                                                                           \
+    if( wxPliVirtualCallback_FindCallback( &m_callback, #METHOD ) )           \
+    {                                                                         \
+        wxPliVirtualCallback_CallCallback( &m_callback,                       \
+                                           G_SCALAR|G_DISCARD );              \
+    } else                                                                    \
+        BASE::METHOD();                                                \
+  }
+
+#define DEF_V_CBACK_BOOL__INT( CLASS, BASE, METHOD ) \
+  bool CLASS::METHOD( int parameter )                                         \
+  {                                                                           \
+    if( wxPliVirtualCallback_FindCallback( &m_callback, #METHOD ) )           \
+    {                                                                         \
+        SV* ret = wxPliVirtualCallback_CallCallback( &m_callback, G_SCALAR,   \
+                                                     "i", parameter );        \
+        bool val = SvTRUE( ret );                                             \
+        SvREFCNT_dec( ret );                                                  \
+        return val;                                                           \
+    } else                                                                    \
+        return BASE::METHOD( parameter );                                     \
+  }
+
+#define DEF_V_CBACK_BOOL__INT_pure( CLASS, BASE, METHOD ) \
+  bool CLASS::METHOD( int parameter )                                         \
+  {                                                                           \
+    if( wxPliVirtualCallback_FindCallback( &m_callback, #METHOD ) )           \
+    {                                                                         \
+        SV* ret = wxPliVirtualCallback_CallCallback( &m_callback, G_SCALAR,   \
+                                                     "i", parameter );        \
+        bool val = SvTRUE( ret );                                             \
+        SvREFCNT_dec( ret );                                                  \
+        return val;                                                           \
+    }                                                                         \
+    return false;                                                             \
+  }
+
+#define DEF_V_CBACK_BOOL__INT_INT( CLASS, BASE, METHOD ) \
+  bool CLASS::METHOD( int param1, int param2 )                                \
+  {                                                                           \
+    if( wxPliVirtualCallback_FindCallback( &m_callback, #METHOD ) )           \
+    {                                                                         \
+        SV* ret = wxPliVirtualCallback_CallCallback( &m_callback, G_SCALAR,   \
+                                                     "ii", param1, param2 );  \
+        bool val = SvTRUE( ret );                                             \
+        SvREFCNT_dec( ret );                                                  \
+        return val;                                                           \
+    } else                                                                    \
+        return BASE::METHOD( param1, param2 );                                \
   }
 
 #endif // _WXPERL_V_CBACK_H
