845f194ec0f03cd6c368b6459bca7dd5b0b88d11
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2002-06-09 08:23:15 +0000

    Added empty prototype to all functions
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@560 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- lib/Wx/_Constants.pm
+++ lib/Wx/_Constants.pm
@@ -40,70 +40,70 @@ sub END {
 # !parser: sub { $_[0] =~ m/^\s*sub\s+(wx\w+)[^\}]*\}\s*(?:\#(.*))?$/ }
 # !package: Wx
 
-sub wxDefaultSize { $_default_size }              # misc
-sub wxDefaultPosition { $_default_position }      # misc
-sub wxDefaultValidator { $_default_validator }    # misc
+sub wxDefaultSize() { $_default_size }              # misc
+sub wxDefaultPosition() { $_default_position }      # misc
+sub wxDefaultValidator() { $_default_validator }    # misc
 
-sub wxNullBitmap { $_null_bitmap }                # bitmap
-sub wxNullIcon { $_null_icon }                    # icon
-sub wxNullColour { $_null_colour }                # colour color
-sub wxNullCursor { $_null_cursor }                # cursor
-sub wxNullFont { $_null_font }                    # font
-sub wxNullPen { $_null_pen }                      # pen
-sub wxNullBrush { $_null_brush }                  # brush
-sub wxNullPalette { $_null_palette }              # palette
-sub wxNullAcceleratorTable { $_null_accelerator }
+sub wxNullBitmap() { $_null_bitmap }                # bitmap
+sub wxNullIcon() { $_null_icon }                    # icon
+sub wxNullColour() { $_null_colour }                # colour color
+sub wxNullCursor() { $_null_cursor }                # cursor
+sub wxNullFont() { $_null_font }                    # font
+sub wxNullPen() { $_null_pen }                      # pen
+sub wxNullBrush() { $_null_brush }                  # brush
+sub wxNullPalette() { $_null_palette }              # palette
+sub wxNullAcceleratorTable() { $_null_accelerator }
 
-sub wxRED { $_colour_red }                        # color colour
-sub wxGREEN { $_colour_green }                    # color colour
-sub wxBLUE { $_colour_blue }                      # color colour
-sub wxBLACK { $_colour_black }                    # color colour
-sub wxWHITE { $_colour_white }                    # color colour
-sub wxCYAN { $_colour_cyan }                      # color colour
-sub wxLIGHT_GREY { $_colour_light_grey }          # color colour
+sub wxRED() { $_colour_red }                        # color colour
+sub wxGREEN() { $_colour_green }                    # color colour
+sub wxBLUE() { $_colour_blue }                      # color colour
+sub wxBLACK() { $_colour_black }                    # color colour
+sub wxWHITE() { $_colour_white }                    # color colour
+sub wxCYAN() { $_colour_cyan }                      # color colour
+sub wxLIGHT_GREY() { $_colour_light_grey }          # color colour
 
-sub wxSTANDARD_CURSOR { $_cursor_cross }          # cursor
-sub wxHOURGLASS_CURSOR { $_cursor_hourglass }     # cursor
-sub wxCROSS_CURSOR { $_cursor_cross }             # cursor
+sub wxSTANDARD_CURSOR() { $_cursor_cross }          # cursor
+sub wxHOURGLASS_CURSOR() { $_cursor_hourglass }     # cursor
+sub wxCROSS_CURSOR() { $_cursor_cross }             # cursor
 
-sub wxNORMAL_FONT { $_font_normal }               # font
-sub wxSMALL_FONT { $_font_small }                 # font
-sub wxITALIC_FONT { $_font_italic }               # font
-sub wxSWISS_FONT { $_font_swiss }                 # font
+sub wxNORMAL_FONT() { $_font_normal }               # font
+sub wxSMALL_FONT() { $_font_small }                 # font
+sub wxITALIC_FONT() { $_font_italic }               # font
+sub wxSWISS_FONT() { $_font_swiss }                 # font
 
-sub wxRED_PEN { $_pen_red }                       # pen
-sub wxGREEN_PEN { $_pen_green }                   # pen
-sub wxCYAN_PEN { $_pen_cyan }                     # pen
-sub wxBLACK_PEN { $_pen_black }                   # pen
-sub wxWHITE_PEN { $_pen_white }                   # pen
-sub wxTRANSPARENT_PEN { $_pen_transparent }       # pen
-sub wxBLACK_DASHED_PEN { $_pen_black_dashed }     # pen
-sub wxGREY_PEN { $_pen_grey }                     # pen
-sub wxMEDIUM_GREY_PEN { $_pen_medium_grey }       # pen
-sub wxLIGHT_GREY_PEN { $_pen_light_grey }         # pen
+sub wxRED_PEN() { $_pen_red }                       # pen
+sub wxGREEN_PEN() { $_pen_green }                   # pen
+sub wxCYAN_PEN() { $_pen_cyan }                     # pen
+sub wxBLACK_PEN() { $_pen_black }                   # pen
+sub wxWHITE_PEN() { $_pen_white }                   # pen
+sub wxTRANSPARENT_PEN() { $_pen_transparent }       # pen
+sub wxBLACK_DASHED_PEN() { $_pen_black_dashed }     # pen
+sub wxGREY_PEN() { $_pen_grey }                     # pen
+sub wxMEDIUM_GREY_PEN() { $_pen_medium_grey }       # pen
+sub wxLIGHT_GREY_PEN() { $_pen_light_grey }         # pen
 
-sub wxBLUE_BRUSH { $_brush_blue }                 # brush
-sub wxGREEN_BRUSH { $_brush_green }               # brush
-sub wxWHITE_BRUSH { $_brush_white }               # brush
-sub wxBLACK_BRUSH { $_brush_black }               # brush
-sub wxGREY_BRUSH { $_brush_grey }                 # brush
-sub wxMEDIUM_GREY_BRUSH { $_brush_medium_grey }   # brush
-sub wxLIGHT_GREY_BRUSH { $_brush_light_grey }     # brush
-sub wxTRANSPARENT_BRUSH { $_brush_transparent }   # brush
-sub wxCYAN_BRUSH { $_brush_cyan }                 # brush
-sub wxRED_BRUSH { $_brush_red }                   # brush
+sub wxBLUE_BRUSH() { $_brush_blue }                 # brush
+sub wxGREEN_BRUSH() { $_brush_green }               # brush
+sub wxWHITE_BRUSH() { $_brush_white }               # brush
+sub wxBLACK_BRUSH() { $_brush_black }               # brush
+sub wxGREY_BRUSH() { $_brush_grey }                 # brush
+sub wxMEDIUM_GREY_BRUSH() { $_brush_medium_grey }   # brush
+sub wxLIGHT_GREY_BRUSH() { $_brush_light_grey }     # brush
+sub wxTRANSPARENT_BRUSH() { $_brush_transparent }   # brush
+sub wxCYAN_BRUSH() { $_brush_cyan }                 # brush
+sub wxRED_BRUSH() { $_brush_red }                   # brush
 
-sub wxFormatInvalid { $_format_invalid }          # dnd
-sub wxTheClipboard { $_clipboard }                # clipboard
+sub wxFormatInvalid() { $_format_invalid }          # dnd
+sub wxTheClipboard() { $_clipboard }                # clipboard
 
-sub wxVERSION_STRING { $_version_string }
+sub wxVERSION_STRING() { $_version_string }
 
-sub wxTheApp { $Wx::App::theapp }
+sub wxTheApp() { $Wx::App::theapp }
 
-sub wxIMAGE_OPTION_BMP_FORMAT { $Wx::_img_bmp_format } # image
-sub wxIMAGE_OPTION_CUR_HOTSPOT_X { $Wx::_img_cur_hotspot_x } # image
-sub wxIMAGE_OPTION_CUR_HOTSPOT_Y { $Wx::_img_cur_hotspot_y } # image
-sub wxIMAGE_OPTION_FILENAME { $Wx::_img_filename } # image
+sub wxIMAGE_OPTION_BMP_FORMAT() { $Wx::_img_bmp_format } # image
+sub wxIMAGE_OPTION_CUR_HOTSPOT_X() { $Wx::_img_cur_hotspot_x } # image
+sub wxIMAGE_OPTION_CUR_HOTSPOT_Y() { $Wx::_img_cur_hotspot_y } # image
+sub wxIMAGE_OPTION_FILENAME() { $Wx::_img_filename } # image
 
 1;
 
