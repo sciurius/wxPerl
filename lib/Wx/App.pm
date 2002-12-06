d96f6d16a80152e745b2f43d1abe6a0d5915b9a8
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2002-12-06 18:58:58 +0000

    Fix error message.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@781 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- lib/Wx/App.pm
+++ lib/Wx/App.pm
@@ -5,7 +5,7 @@
 ## Modified by:
 ## Created:     25/11/2000
 ## RCS-ID:      
-## Copyright:   (c) 2000 Mattia Barbon
+## Copyright:   (c) 2000-2002 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
 #############################################################################
@@ -33,7 +33,7 @@ sub new {
   }
 
   Wx::_App::Start($this,$this->can('OnInit')) ||
-      Wx::_croak( 'OnInit must return a return value' );
+      Wx::_croak( 'OnInit must return a true return value' );
 
   $this;
 }
