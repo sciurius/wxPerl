6bcd5f7476dae51d81dc5a0579c7cdc0f1de8525
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2002-12-24 18:08:37 +0000

    Part of the new build system.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@793 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

---
+++ script/copy_files.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+#############################################################################
+## Name:        copy_files.pl
+## Purpose:     Simple wrapper around ExtUtils::Install
+## Author:      Mattia Barbon
+## Modified by:
+## Created:     16/12/2002
+## RCS-ID:      
+## Copyright:   (c) 2002 Mattia Barbon
+## Licence:     This program is free software; you can redistribute it and/or
+##              modify it under the same terms as Perl itself
+#############################################################################
+
+use strict;
+use ExtUtils::Install 'pm_to_blib';
+
+my $file = shift;
+
+die "File '$file' does not exist" unless -f $file;
+my $files = do $file;
+
+die "Error loading options: $@" unless $files;
+#die %$files;
+pm_to_blib( $files,
+            'blib', # dummy argument, but required
+            '' );   # idem
+
+exit 0;
+
+# local variables:
+# mode: cperl
+# end:
