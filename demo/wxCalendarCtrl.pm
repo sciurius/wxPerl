b1d27a4d7622226a62b742a26f36b1728e101097
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2003-04-22 19:24:41 +0000

    Added Wx::CalendarDateAttr + demo.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@911 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- demo/wxCalendarCtrl.pm
+++ demo/wxCalendarCtrl.pm
@@ -4,8 +4,8 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     11/10/2002
-## RCS-ID:      
-## Copyright:   (c) 2002 Mattia Barbon
+## RCS-ID:      $Id: wxCalendarCtrl.pm,v 1.3 2003/04/22 19:24:39 mbarbon Exp $
+## Copyright:   (c) 2002-2003 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
 #############################################################################
@@ -40,16 +40,16 @@ package CalendarCtrlDemoWin;
 use base 'Wx::Panel';
 use Wx::Calendar;
 
-use Wx qw(:sizer wxDefaultPosition wxDefaultSize);
+use Wx qw(:sizer :calendar wxDefaultPosition wxDefaultSize wxRED wxBLUE);
 use Wx::Event qw(EVT_CALENDAR_SEL_CHANGED);
 
 sub new {
   my $class = shift;
   my $this = $class->SUPER::new( $_[0], -1 );
-  #                                    8 Jan 1979
 
   my $sizer = Wx::BoxSizer->new( wxVERTICAL );
 
+  #                                    8 Jan 1979
   my $date = Wx::DateTime->newFromDMY( 8, 0, 1979 );
 
   my $calendar = Wx::CalendarCtrl->new( $this, -1, $date );
@@ -61,6 +61,16 @@ sub new {
 
   $calendar->EnableYearChange;
   $calendar->EnableMonthChange;
+
+  # test attributes
+  my $attr = Wx::CalendarDateAttr->new;
+  $attr->SetTextColour( wxRED );
+  $attr->SetBorderColour( wxBLUE );
+  $attr->SetBorder( wxCAL_BORDER_ROUND );
+
+  $calendar->SetAttr( 2, $attr );
+  $calendar->SetAttr( 3, $attr );
+  $calendar->SetAttr( 4, $attr );
 
   EVT_CALENDAR_SEL_CHANGED( $this, $calendar,
                             sub {
--- ext/calendar/Calendar.xs
+++ ext/calendar/Calendar.xs
@@ -1,10 +1,10 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        DateTime.xs
+// Name:        ext/calendar/Calendar.xs
 // Purpose:     XS for Wx::DateTime
 // Author:      Mattia Barbon
 // Modified by:
 // Created:     22/ 9/2002
-// RCS-ID:      
+// RCS-ID:      $Id: Calendar.xs,v 1.3 2003/04/22 19:24:39 mbarbon Exp $
 // Copyright:   (c) 2002 Mattia Barbon
 // Licence:     This program is free software; you can redistribute it and/or
 //              modify it under the same terms as Perl itself
@@ -23,6 +23,7 @@ BOOT:
   INIT_PLI_HELPERS( wx_pli_helpers );
 
 INCLUDE: perl ../../script/xsubppp.pl --typemap=../../typemap.xsp XS/CalendarCtrl.xsp |
+INCLUDE: perl ../../script/xsubppp.pl --typemap=../../typemap.xsp XS/CalendarDateAttr.xsp |
 
 #  //FIXME//tricky
 #if defined(__WXMSW__)
