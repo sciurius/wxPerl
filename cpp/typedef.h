c881ebfebe052df1c3dabdccfdb0b619e6262f35
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2003-05-27 20:16:01 +0000

    Updates to match other implementation changes.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@992 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- cpp/typedef.h
+++ cpp/typedef.h
@@ -4,7 +4,7 @@
 // Author:      Mattia Barbon
 // Modified by:
 // Created:     29/10/2000
-// RCS-ID:      $Id: typedef.h,v 1.42 2003/05/05 20:38:41 mbarbon Exp $
+// RCS-ID:      $Id: typedef.h,v 1.43 2003/05/27 20:16:01 mbarbon Exp $
 // Copyright:   (c) 2000-2003 Mattia Barbon
 // Licence:     This program is free software; you can redistribute it and/or
 //              modify it under the same terms as Perl itself
@@ -75,13 +75,11 @@ FD_TD( AcceleratorEntry );
 FD_TD( AcceleratorTable );
 FD_TD( App );
 FD_TD( Bitmap );
-FD_TD( BitmapButton );
 FD_TD( BitmapHandler );
 FD_TD( BoxSizer );
 FD_TD( Brush );
 FD_TD( BusyCursor );
 FD_TD( BusyInfo );
-FD_TD( Button );
 FD_TD( Caret );
 FD_TD( CaretSuspend );
 FD_TD( CheckBox );
@@ -140,7 +138,6 @@ FD_TD( ImageList );
 FD_TD( IndividualLayoutConstraint );
 FD_TDS( LanguageInfo )
 FD_TD( LayoutConstraints );
-FD_TD( ListBox );
 FD_TD( ListCtrl );
 FD_TD( ListItem );
 FD_TD( ListItemAttr );
