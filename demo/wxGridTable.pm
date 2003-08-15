b639f7f750875552a6d984c8c40ec4bb1bba6ab2
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2003-08-15 21:53:17 +0000

    Deleted commented out code. Demonstrate a couple more methods.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@1066 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- demo/wxGridTable.pm
+++ demo/wxGridTable.pm
@@ -4,7 +4,7 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     05/08/2003
-## RCS-ID:      $Id: wxGridTable.pm,v 1.1 2003/08/05 17:23:47 mbarbon Exp $
+## RCS-ID:      $Id: wxGridTable.pm,v 1.2 2003/08/15 21:53:17 mbarbon Exp $
 ## Copyright:   (c) 2003 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -49,15 +49,43 @@ sub GetNumberCols { 100000 }
 sub IsEmptyCell { 0 }
 
 sub GetValue {
-  my( $this, $x, $y ) = @_;
+  my( $this, $y, $x ) = @_;
 
-  return "($x, $y)";
+  return "($y, $x)";
 }
 
 sub SetValue {
   my( $this, $x, $y, $value ) = @_;
 
   die "Read-Only table";
+}
+
+sub GetTypeName {
+  my( $this, $r, $c ) = @_;
+
+  return $c == 0 ? 'bool' :
+         $c == 1 ? 'double' :
+                   'string';
+}
+
+sub CanGetValueAs {
+  my( $this, $r, $c, $type ) = @_;
+
+  return $c == 0 ? $type eq 'bool' :
+         $c == 1 ? $type eq 'double' :
+                   $type eq 'string';
+}
+
+sub GetValueAsBool {
+  my( $this, $r, $c ) = @_;
+
+  return $r % 2;
+}
+
+sub GetValueAsDouble {
+  my( $this, $r, $c ) = @_;
+
+  return $r + $c / 1000;
 }
 
 package GridTableDemoWindow;
@@ -73,121 +101,7 @@ sub new {
 
   $this->SetTable( $table );
 
-=pod
-
-  $this->CreateGrid( 3, 7 );
-  # set every cell read-only
-  for my $x ( 1 .. 7 ) {
-    for my $y ( 1 .. 3 ) {
-      $this->SetReadOnly( $x, $y );
-    }
-  }
-
-  $this->SetColSize( 0, 20 );
-  $this->SetColSize( 1, 150 );
-  $this->SetColSize( 2, 100 );
-  $this->SetColSize( 3, 20 );
-  $this->SetColSize( 4, 150 );
-  $this->SetColSize( 5, 100 );
-  $this->SetColSize( 6, 20 );
-
-  $this->SetCellValue( 1, 1, "Custom editor" );
-  $this->SetCellValue( 1, 2, "Some value" );
-  $this->SetCellEditor( 1, 2, MyCellEditor->new );
-  $this->SetReadOnly( 1, 2, 0 );
-
-  $this->SetCellValue( 1, 4, "Custom renderer" );
-  $this->SetCellValue( 1, 5, "SoMe TeXt!" );
-  $this->SetCellRenderer( 1, 5, MyCellRenderer->new );
-  $this->SetReadOnly( 1, 5, 0 );
-
-=cut
-
   return $this;
-}
-
-package MyCellRenderer;
-
-use strict;
-use base 'Wx::PlGridCellRenderer';
-use Wx qw(wxBLACK_PEN wxWHITE_BRUSH);
-
-sub Draw {
-  my( $self, $grid, $attr, $dc, $rect, $row, $col, $sel ) = ( shift, @_ );
-
-  $self->SUPER::Draw( @_ );
-
-  $dc->SetPen( wxBLACK_PEN );
-  $dc->SetBrush( wxWHITE_BRUSH );
-  $dc->DrawEllipse( $rect->x, $rect->y, $rect->width, $rect->height );
-  $dc->DrawText( $grid->GetCellValue( $row, $col ), $rect->x, $rect->y );
-}
-
-sub Clone {
-  my $self = shift;
-
-  return $self->new;
-}
-
-package Wx::GridWindow;
-
-use base 'Wx::Grid';
-
-package MyCellEditor;
-
-use strict;
-use base 'Wx::PlGridCellEditor';
-
-sub new {
-  my $class = shift;
-  my $self = $class->SUPER::new;
-
-  return $self;
-}
-
-sub Create {
-  my( $self, $parent, $id, $evthandler ) = @_;
-
-  $self->{text_ctrl} = Wx::TextCtrl->new( $parent, $id, 'Default value' );
-
-  Wx::LogMessage( 'Create called' );
-}
-
-sub Destroy {
-  my $self = shift;
-
-  $self->{text_ctrl}->Destroy if $self->{text_ctrl};
-}
-
-sub SetSize {
-  my( $self, $size ) = @_;
-
-  $self->{text_ctrl}->SetSize( $size );
-
-  Wx::LogMessage( 'SetSize called' );
-}
-
-sub Show {
-  my( $self, $show, $attr ) = @_;
-
-  $self->{text_ctrl}->Show( $show );
-
-  Wx::LogMessage( 'Show called' );
-}
-
-sub EndEdit {
-  my( $self, $row, $col, $grid ) = @_;
-
-  my $value = '>> ' . $self->{text_ctrl}->GetValue . ' <<';
-  my $oldValue = $grid->GetCellValue( $row, $col );
-
-  my $changed =  $value ne $oldValue;
-
-  if( $changed ) { $grid->SetCellValue( $row, $col, $value ) }
-
-  Wx::LogMessage( 'EndEdit called' );
-
-  return $changed;
 }
 
 1;
