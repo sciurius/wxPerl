c85a855257fc47804cd866d08b67d67848713c8d
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2003-12-26 11:14:14 +0000

    wxWindows 2.5 fix.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@1196 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- samples/controls/controls.pl
+++ samples/controls/controls.pl
@@ -5,7 +5,7 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     29/10/2000
-## RCS-ID:      $Id: controls.pl,v 1.10 2003/05/04 17:35:09 mbarbon Exp $
+## RCS-ID:      $Id: controls.pl,v 1.11 2003/12/26 11:14:14 mbarbon Exp $
 ## Copyright:   (c) 2000, 2003 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -112,7 +112,6 @@ sub new {
     $this->{OLDLOG} =
       Wx::Log::SetActiveTarget( Wx::LogTextCtrl->new( $this->{TEXT} ) );
 
-    Wx::LogTrace( "Wx::LogTrace" );
     Wx::LogTraceMask( 'test', "You can't see this!" );
     Wx::Log::AddTraceMask( 'test' );
     Wx::LogTraceMask( "Wx::LogTraceMask" );
