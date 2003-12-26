1c74a2de31cf4f52c4edb8a0d0ed99c0b6ae8afc
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2003-12-26 11:21:10 +0000

    Added (incomplete) wrappers for wxHtmlParser, wxHtmlTagHandler and related classes, plus wrappers for custom tag handlers. wxHtmlTag API is still almost inexistent.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@1197 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- ext/html/lib/Wx/Html.pm
+++ ext/html/lib/Wx/Html.pm
@@ -1,11 +1,11 @@
 #############################################################################
-## Name:        Html.pm
-## Purpose:     Wx::Html ( pulls in all Wx::Html* stuff )
+## Name:        ext/html/lib/Wx/Html.pm
+## Purpose:     Wx::Html (pulls in all Wx::Html* stuff)
 ## Author:      Mattia Barbon
 ## Modified by:
-## Created:     17/ 3/2001
-## RCS-ID:      
-## Copyright:   (c) 2001-2002 Mattia Barbon
+## Created:     17/03/2001
+## RCS-ID:      $Id: Html.pm,v 1.6 2003/12/26 11:21:10 mbarbon Exp $
+## Copyright:   (c) 2001-2003 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
 #############################################################################
@@ -28,11 +28,25 @@ Wx::wx_boot( 'Wx::Html', $VERSION );
 
 no strict;
 
-package Wx::HtmlWindow;   @ISA = qw(Wx::ScrolledWindow);
+package Wx::HtmlWindow;         @ISA = qw(Wx::ScrolledWindow);
 package Wx::HtmlHelpController; @ISA = qw(Wx::HelpControllerBase);
+package Wx::HtmlParser;
+package Wx::HtmlWinParser;      @ISA = qw(Wx::HtmlParser);
+package Wx::HtmlTag;
+package Wx::PlHtmlTag;          @ISA = qw(Wx::HtmlTag);
+package Wx::HtmlTagHandler;
+package Wx::PlHtmlTagHandler;   @ISA = qw(Wx::HtmlTagHandler);
+package Wx::HtmlWinTagHandler;  @ISA = qw(Wx::HtmlTagHandler);
+package Wx::PlHtmlWinTagHandler;@ISA = qw(Wx::HtmlWinTagHandler);
+package Wx::HtmlCell;
+package Wx::HtmlWordCell;       @ISA = qw(Wx::HtmlCell);
+package Wx::HtmlContainerCell;  @ISA = qw(Wx::HtmlCell);
+package Wx::HtmlFontCell;       @ISA = qw(Wx::HtmlCell);
+package Wx::HtmlColourCell;     @ISA = qw(Wx::HtmlCell);
+package Wx::HtmlWidgetCell;     @ISA = qw(Wx::HtmlCell);
 
 1;
 
-# Local variables: #
-# mode: cperl #
-# End: #
+# Local variables:
+# mode: cperl
+# End:
