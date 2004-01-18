b93708b696f79b87329f57e237aeb288ff212156
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2004-01-18 08:13:34 +0000

    Build and install 'wxPerl' executable automatically on Mac OS X.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@1202 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- build/Wx/build/MakeMaker/MacOSX_GCC.pm
+++ build/Wx/build/MakeMaker/MacOSX_GCC.pm
@@ -3,6 +3,32 @@ package Wx::build::MakeMaker::MacOSX_GCC;
 use strict;
 use base 'Wx::build::MakeMaker::Any_wx_config';
 
+sub configure_core {
+  my $this = shift;
+  my %config = $this->SUPER::configure_core( @_ );
+
+  $config{depend}{'$(INST_STATIC)'} .= ' $(INST_BIN)/wxPerl';
+  $config{depend}{'$(INST_DYNAMIC)'} .= ' $(INST_BIN)/wxPerl';
+
+  return %config;
+}
+
+sub postamble_core {
+  my $this = shift;
+  my $text = $this->SUPER::postamble_core( @_ );
+
+  $text .= sprintf <<'EOT', ;
+
+$(INST_BIN)/wxPerl :
+	mkdir -p $(INST_BIN)
+	cp $(PERL) $(INST_BIN)/wxPerl
+	`wx-config --rezflags` $(INST_BIN)/wxPerl
+
+EOT
+
+  return $text;
+}
+
 1;
 
 # local variables:
