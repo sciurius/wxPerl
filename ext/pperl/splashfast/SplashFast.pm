e426ce30a1f251128fec30b765978868910e3b19
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2004-06-20 08:51:05 +0000

    Update this version, too.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@1292 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- ext/pperl/splashfast/SplashFast.pm
+++ ext/pperl/splashfast/SplashFast.pm
@@ -4,7 +4,7 @@
 ## Author:      Graciliano M. P.
 ## Modified by:
 ## Created:     30/06/2002
-## RCS-ID:      $Id: SplashFast.pm,v 1.13 2004/02/28 22:59:07 mbarbon Exp $
+## RCS-ID:      $Id: SplashFast.pm,v 1.14 2004/06/20 08:51:05 mbarbon Exp $
 ## Copyright:   (c) 2002-2003 Graciliano M. P.
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -62,7 +62,7 @@ package Wx ;
 
 use vars qw($VERSION);
 
-$VERSION = '0.19'; # bootstrap will catch wrong versions
+$VERSION = '0.20'; # bootstrap will catch wrong versions
 
 sub wxPL_STATIC();
 sub _wx_boot($$) {
