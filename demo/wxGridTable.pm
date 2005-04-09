9563556b48d26c48b16a3c2e2526f348780bbdb8
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2005-04-09 13:25:43 +0000

    Show current selection as in demo/wxGrid.pm.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@1468 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- demo/wxGridTable.pm
+++ demo/wxGridTable.pm
@@ -4,7 +4,7 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     05/08/2003
-## RCS-ID:      $Id: wxGridTable.pm,v 1.2 2003/08/15 21:53:17 mbarbon Exp $
+## RCS-ID:      $Id: wxGridTable.pm,v 1.3 2005/04/09 13:25:43 mbarbon Exp $
 ## Copyright:   (c) 2003 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -93,6 +93,13 @@ package GridTableDemoWindow;
 use strict;
 use base 'Wx::Grid';
 
+use Wx::Event qw(EVT_GRID_CELL_LEFT_CLICK EVT_GRID_CELL_RIGHT_CLICK
+    EVT_GRID_CELL_LEFT_DCLICK EVT_GRID_CELL_RIGHT_DCLICK
+    EVT_GRID_LABEL_LEFT_CLICK EVT_GRID_LABEL_RIGHT_CLICK
+    EVT_GRID_LABEL_LEFT_DCLICK EVT_GRID_LABEL_RIGHT_DCLICK
+    EVT_GRID_ROW_SIZE EVT_GRID_COL_SIZE EVT_GRID_RANGE_SELECT
+    EVT_GRID_CELL_CHANGE EVT_GRID_SELECT_CELL);
+
 sub new {
   my $class = shift;
   my $this = $class->SUPER::new( $_[0], -1 );
@@ -101,7 +108,106 @@ sub new {
 
   $this->SetTable( $table );
 
+  EVT_GRID_CELL_LEFT_CLICK( $this, c_log_skip( "Cell left click" ) );
+  EVT_GRID_CELL_RIGHT_CLICK( $this, c_log_skip( "Cell right click" ) );
+  EVT_GRID_CELL_LEFT_DCLICK( $this, c_log_skip( "Cell left double click" ) );
+  EVT_GRID_CELL_RIGHT_DCLICK( $this, c_log_skip( "Cell right double click" ) );
+  EVT_GRID_LABEL_LEFT_CLICK( $this, c_log_skip( "Label left click" ) );
+  EVT_GRID_LABEL_RIGHT_CLICK( $this, c_log_skip( "Label right click" ) );
+  EVT_GRID_LABEL_LEFT_DCLICK( $this, c_log_skip( "Label left double click" ) );
+  EVT_GRID_LABEL_RIGHT_DCLICK( $this, c_log_skip( "Label right double click" ) );
+
+  EVT_GRID_ROW_SIZE( $this, sub {
+                       Wx::LogMessage( "%s %s", "Row size", GS2S( $_[1] ) );
+                       $_[1]->Skip;
+                     } );
+  EVT_GRID_COL_SIZE( $this, sub {
+                       Wx::LogMessage( "%s %s", "Col size", GS2S( $_[1] ) );
+                       $_[1]->Skip;
+                     } );
+
+  EVT_GRID_RANGE_SELECT( $this, sub {
+                           Wx::LogMessage( "Range %sselect (%d, %d, %d, %d)",
+                                           ( $_[1]->Selecting ? '' : 'de' ),
+                                           $_[1]->GetLeftCol, $_[1]->GetTopRow,
+                                           $_[1]->GetRightCol,
+                                           $_[1]->GetBottomRow );
+                           $_[0]->ShowSelections;
+                           $_[1]->Skip;
+                         } );
+  EVT_GRID_CELL_CHANGE( $this, c_log_skip( "Cell content changed" ) );
+  EVT_GRID_SELECT_CELL( $this, c_log_skip( "Cell select" ) );
+
   return $this;
+}
+
+sub ShowSelections {
+    my $this = shift;
+
+    my @cells = $this->GetSelectedCells;
+    if( @cells ) {
+        Wx::LogMessage( "Cells %s selected", join ', ',
+                                                  map { "(" . $_->GetCol .
+                                                        ", " . $_->GetRow . ")"
+                                                       } @cells );
+    } else {
+        Wx::LogMessage( "No cells selected" );
+    }
+
+    my @tl = $this->GetSelectionBlockTopLeft;
+    my @br = $this->GetSelectionBlockBottomRight;
+    if( @tl && @br ) {
+        Wx::LogMessage( "Blocks %s selected",
+                        join ', ',
+                        map { "(" . $tl[$_]->GetCol .
+                              ", " . $tl[$_]->GetRow . "-" .
+                              $br[$_]->GetCol . ", " .
+                              $br[$_]->GetRow . ")"
+                            } 0 .. $#tl );
+    } else {
+        Wx::LogMessage( "No blocks selected" );
+    }
+
+    my @rows = $this->GetSelectedRows;
+    if( @rows ) {
+        Wx::LogMessage( "Rows %s selected", join ', ', @rows );
+    } else {
+        Wx::LogMessage( "No rows selected" );
+    }
+
+    my @cols = $this->GetSelectedCols;
+    if( @cols ) {
+        Wx::LogMessage( "Columns %s selected", join ', ', @cols );
+    } else {
+        Wx::LogMessage( "No columns selected" );
+    }
+}
+
+# pretty printer for Wx::GridEvent
+sub G2S {
+  my $event = shift;
+  my( $x, $y ) = ( $event->GetCol, $event->GetRow );
+
+  return "( $x, $y )";
+}
+
+# prety printer for Wx::GridSizeEvent
+sub GS2S {
+  my $event = shift;
+  my $roc = $event->GetRowOrCol;
+
+  return "( $roc )";
+}
+
+# creates an anonymous sub that logs and skips any grid event
+sub c_log_skip {
+  my $text = shift;
+
+  return sub {
+    Wx::LogMessage( "%s %s", $text, G2S( $_[1] ) );
+    $_[0]->ShowSelections;
+    $_[1]->Skip;
+  };
 }
 
 1;
