7d46b7f7221845228fd60e37f945cb7026d1592b
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2005-07-24 17:47:37 +0000

    Call base class implementation in wxPlGridCellEditor if not overridden.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@1526 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- ext/grid/cpp/editor.h
+++ ext/grid/cpp/editor.h
@@ -4,7 +4,7 @@
 // Author:      Mattia Barbon
 // Modified by:
 // Created:     28/05/2003
-// RCS-ID:      $Id: editor.h,v 1.7 2005/02/26 11:31:02 mbarbon Exp $
+// RCS-ID:      $Id: editor.h,v 1.8 2005/07/24 17:47:37 mbarbon Exp $
 // Copyright:   (c) 2003-2005 Mattia Barbon
 // Licence:     This program is free software; you can redistribute it and/or
 //              modify it under the same terms as Perl itself
@@ -122,7 +122,8 @@ public:
                                                G_DISCARD|G_SCALAR,
                                                "o", new wxRect( rect ),
                                                "Wx::Rect" );
-        }
+        } else
+            wxGridCellEditor::SetSize( rect );
     }
 
     void Show( bool show, wxGridCellAttr* attr )
@@ -145,10 +146,11 @@ public:
 
             FREETMPS;
             LEAVE;
-        }
+        } else
+            wxGridCellEditor::Show( show, attr );
     }
 
-    void PaintBackground(  const wxRect& rect, wxGridCellAttr* attr )
+    void PaintBackground( const wxRect& rect, wxGridCellAttr* attr )
     {
         dTHX;
 
@@ -169,7 +171,8 @@ public:
 
             FREETMPS;
             LEAVE;
-        }
+        } else
+            wxGridCellEditor::PaintBackground( rect, attr );
     }
 
     virtual wxGridCellEditor* Clone() const
