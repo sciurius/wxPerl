c11a3f50a6133c2d188f3277ccf51eb7fbb0eb1f
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2006-04-22 21:25:33 +0000

    Release 0.49_03 and 0.49_04.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@1661 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- Wx.pm
+++ Wx.pm
@@ -4,7 +4,7 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     01/10/2000
-## RCS-ID:      $Id: Wx.pm,v 1.88 2006/04/16 18:51:08 mbarbon Exp $
+## RCS-ID:      $Id: Wx.pm,v 1.89 2006/04/22 21:25:33 mbarbon Exp $
 ## Copyright:   (c) 2000-2006 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -21,7 +21,7 @@ use vars qw(@ISA $VERSION $XS_VERSION $AUTOLOAD @EXPORT_OK %EXPORT_TAGS
 $_msw = 1; $_gtk = 2; $_motif = 3; $_mac = 4; $_x11 = 5;
 
 @ISA = qw(Exporter);
-$VERSION = '0.49_02';
+$VERSION = '0.49_04';
 $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
