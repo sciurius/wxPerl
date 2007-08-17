13e1a0238ab43e70943b2d30f824af82b44c6973
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-08-17 19:26:19 +0000

    Add check that Mac OS X deployment target must be greater than 10.3 (lower values are unsupported by recent wxWidgets anyway).
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@2169 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- build/Wx/build/MakeMaker/MacOSX_GCC.pm
+++ build/Wx/build/MakeMaker/MacOSX_GCC.pm
@@ -4,6 +4,9 @@ use strict;
 use base 'Wx::build::MakeMaker::Any_wx_config';
 use Wx::build::Utils qw(write_string);
 
+die "Please set MACOSX_DEPLOYMENT_TARGET to 10.3 or above"
+    if $ENV{MACOSX_DEPLOYMENT_TARGET} < 10.3;
+
 sub configure_core {
   my $this = shift;
   my %config = $this->SUPER::configure_core( @_ );
