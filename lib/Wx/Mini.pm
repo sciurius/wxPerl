06d17099d9505ead08a57c7c1a60b95674ceab1f
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2008-09-03 21:12:24 +0000

    Starting wxPerl 0.87.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@2461 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- lib/Wx/Mini.pm
+++ lib/Wx/Mini.pm
@@ -12,7 +12,7 @@ our $alien_key = 'XXXALIENXXX';
     $Wx::dlls = XXXDLLSXXX;
 }
 
-$VERSION = '0.86'; # bootstrap will catch wrong versions
+$VERSION = '0.87'; # bootstrap will catch wrong versions
 $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
