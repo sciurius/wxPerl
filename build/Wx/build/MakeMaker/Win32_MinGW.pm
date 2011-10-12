82f2239eeeed36fe89923bb6d6cf7171a3984dba
mdootson <mdootson@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2011-10-12 23:16:04 +0000

    Wx - support several mingw compilers on 2.9.2 and restore VC6 / 2003 Platform SDK builds for 2.8.12
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@3092 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- build/Wx/build/MakeMaker/Win32_MinGW.pm
+++ build/Wx/build/MakeMaker/Win32_MinGW.pm
@@ -3,9 +3,20 @@ package Wx::build::MakeMaker::Win32_MinGW;
 use strict;
 use Wx::build::Utils qw(path_search);
 use base 'Wx::build::MakeMaker::Win32';
+use Config;
 
 sub _res_file { 'Wx_res.o' }
-sub _res_command { 'windres --include-dir %incdir %src %dest' }
+
+sub _res_command { 
+    # specify windres target for xcompilers
+    my $format = ( $Config{ptrsize} == 8 ) ? 'pe-x86-64' : 'pe-i386';
+    
+    # for info, this is the pe format minus mz headers which is what you
+    # want for object files. Exectuables and dlls get 'pei-x86-64' or 'pei-i386'.
+    
+	return qq(windres --target $format ) . '--include-dir %incdir %src %dest';
+}
+
 sub _strip_command {
   return <<EOT;
 	attrib -r blib\\arch\\auto\\Wx\\*.dll
@@ -28,6 +39,7 @@ sub dynamic_lib {
   
   my $gppcmdout = qx(g++ -static-libstdc++ 2>&1);
   my $ldflags = ($gppcmdout =~ /unrecognized option/) ? '-shared' : '-shared -static-libstdc++';
+  $ldflags .= ( $Config{ptrsize} == 8 ) ? ' -m64' : ' -m32';  
 
   $text =~ s{(?:^\s+(?:dlltool|\$\(LD\)).*\n)+}
     {\tg++ $ldflags $strip -o \$@ \$(LDFROM) \$(MYEXTLIB) \$(PERL_ARCHIVE) \$(LDLOADLIBS) \$(BASEEXT).def\n}m;
