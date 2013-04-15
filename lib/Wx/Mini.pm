08f7138f6de0eeaaafeb2ef0660b3bd537a4b5d4
mdootson <mdootson@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2013-04-15 00:00:55 +0000

    Bump dev version to 0.9922
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/wxPerl/trunk@3476 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- lib/Wx/Mini.pm
+++ lib/Wx/Mini.pm
@@ -12,7 +12,7 @@ our $alien_key = 'XXXALIENXXX';
     $Wx::dlls = XXXDLLSXXX;
 }
 
-$VERSION = '0.9921'; # bootstrap will catch wrong versions
+$VERSION = '0.9922'; # bootstrap will catch wrong versions
 $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
